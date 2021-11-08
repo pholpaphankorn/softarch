24dc5a457d4a05cec220251c549e6e4f70f0957b
unknown <phol12711@outlook.com>
2021-11-08 15:33:32 +0700

    change sort method

--- lab_behavior_design_pattern_2/src/com/company/NameSortStrategy.java
+++ lab_behavior_design_pattern_2/src/com/company/NameSortStrategy.java
@@ -1,18 +1,13 @@
 package com.company;
 
-import java.util.Collections;
+
 import java.util.Comparator;
 import java.util.List;
 
 public class NameSortStrategy implements SortStrategy {
     @Override
     public void customSort(List<Person> people) {
-        Collections.sort(people, new Comparator<Person>() {
-            @Override
-            public int compare(Person p1, Person p2) {
-                return p1.getName().compareTo(p2.getName());
-            }
-        });
+        people.sort(Comparator.comparing(Person::getName));
     }
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
 
--- lab_behavior_design_pattern_2/src/com/company/WeightStrategy.java
+++ lab_behavior_design_pattern_2/src/com/company/WeightStrategy.java
@@ -1,18 +1,13 @@
 package com.company;
 
-import java.util.Collections;
+
 import java.util.Comparator;
 import java.util.List;
 
 public class WeightStrategy implements SortStrategy {
     @Override
     public void customSort(List<Person> people) {
-        Collections.sort(people, new Comparator<Person>() {
-            @Override
-            public int compare(Person p1, Person p2) {
-                return p1.getWeight() - p2.getWeight();
-            }
-        });
+        people.sort(Comparator.comparing(Person::getWeight));
     }
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
 
